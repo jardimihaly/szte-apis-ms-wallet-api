11cb074151b047538106ac5970b0a2eb53ed9e37
Járdi Mihály <jardi.m@outlook.com>
2019-11-18 19:12:38 +0100

    re-added files

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = require('express')();
 module.exports = app; // for testing
 
 var config = {
-  appRoot: __dirname // required config
+  appRoot: __dirname, // required config
 };
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
