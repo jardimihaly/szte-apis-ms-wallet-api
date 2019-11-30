9a0572e28dec0cc81f8d8ddff31583e0ded6fe91
Járdi Mihály <jardi.m@outlook.com>
2019-11-30 11:33:46 +0100

    Added actions to creditcard and payment controllers.

---
+++ api/controllers/creditcard.js
@@ -0,0 +1,38 @@
+function listCards(req, res) {
+
+}
+
+function listCardsAsync(req, res) {
+    
+}
+
+function storeCard(req, res) {
+
+}
+
+function storeCardAsync(req, res) {
+    
+}
+
+function updateCard(req, res) {
+
+}
+
+function updateCardAsync(req, res) {
+    
+}
+
+function removeCard(req, res) {
+
+}
+
+function removeCardAsync(req, res) {
+    
+}
+
+module.exports = {
+    listCards,
+    storeCard,
+    updateCard,
+    removeCard
+}
---
+++ api/controllers/payment.js
@@ -0,0 +1,29 @@
+function genericPayment(req, res) {
+    genericPaymentAsync(req, res);
+}
+
+function genericPaymentAsync(req, res) {
+
+}
+
+function vignettePurchase(req, res) {
+    vignettePurchaseAsync(req, res);
+}
+
+function vignettePurchaseAsync(req, res) {
+
+}
+
+function listPayments(req, res) {
+    listPaymentsAsync(req, res);
+}
+
+function listPaymentsAsync(req, res) {
+
+}
+
+module.exports = {
+    genericPayment,
+    vignettePurchase,
+    listPayments
+}
