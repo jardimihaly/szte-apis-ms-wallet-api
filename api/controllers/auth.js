74cdcfc767e212b12b4dafc5fac154ac091d9ae9
Járdi Mihály <jardi.m@outlook.com>
2019-11-30 11:19:32 +0100

    Added actions to auth controller.

---
+++ api/controllers/auth.js
@@ -0,0 +1,20 @@
+function signup(req, res) {
+    signupAsync(req, res);
+}
+
+function signupAsync(req, res) {
+    // TODO
+}
+
+function login(req, res) {
+    loginAsync(req, res);
+}
+
+function loginAsync(req, res) {
+    // TODO   
+}
+
+module.exports = {
+    signup,
+    login
+}
