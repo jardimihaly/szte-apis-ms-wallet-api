72e4ee428fe2c2c6e4373439e9954c92864b6482
Járdi Mihály <jardi.m@outlook.com>
2019-11-30 15:10:39 +0100

    Vendor controller finished

--- api/controllers/vendor.js
+++ api/controllers/vendor.js
@@ -1,11 +1,29 @@
+const { readFile } = require('fs');
+const { promisify } = require('util');
+const path = require('path');
+
+const readFileAsync = promisify(readFile);
+
 function listVendors(req, res)
 {
     listVendorsAsync(req, res);
 }
 
-function listVendorsAsync(req, res)
+async function listVendorsAsync(req, res)
 {
-
+    let vendors = JSON.parse(
+        (
+            await readFileAsync(
+                path.resolve(
+                    __dirname, 
+                    '../', 
+                    'vendors.json'
+                )
+            )
+        ).toString()
+    );
+    
+    res.json(vendors);
 }
 
 module.exports = {
